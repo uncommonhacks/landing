b9fa27d98717ca46cf853aca9424980d3c8d40ca
laudiacay <laudiacay@gmail.com>
2017-10-20 17:59:16 -0500

    sliding back down when mouse leaves works now

--- js/moving.js
+++ js/moving.js
@@ -1,18 +1,25 @@
 $(document).ready(function(){
-  $("#make-side").hover(function(){
+  $("#make-side").hover(
+  function(){
     $("#make-back-side").slideUp(300);
     $("#make").show(300);
+  },
+  function(){
+    $("#make").hide(300);
+    $("#make-back-side").slideDown(300);
+  }
+  );
+
+  $("#hack-side").hover(
+  function(){
+    $("#hack-back-side").slideUp(300);
+    $("#hack").show(300);
+  },
+  function(){
     $("#hack").hide(300);
     $("#hack-back-side").slideDown(300);
-
-  });
-
-  $("#hack-side").hover(function(){
-    $("#hack-back-side").slideUp(300);
-    $("#make").hide(300);
-    $("#hack").show(300);
-    $("#make-back-side").slideDown(300);
-  });
+  }
+  );
 });
 
 /* document.getElementById('hack-side').addEventListener("click", function() {
