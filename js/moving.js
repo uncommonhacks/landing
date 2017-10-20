fcb9baae67883f44f1be5bb8bd7f97f835a64dcc
George Saieed <saieed.george0@gmail.com>
2017-10-19 19:51:14 -0500

    update landing page

--- js/moving.js
+++ js/moving.js
@@ -1,4 +1,20 @@
+$(document).ready(function(){
+  $("#make-side").hover(function(){
+    $("#make-back-side").slideUp(300);
+    $("#make").show(300);
+    $("#hack").hide(300);
+    $("#hack-back-side").slideDown(300);
 
-document.getElementById('hack-side').addEventListener("click", function() {
+  });
+
+  $("#hack-side").hover(function(){
+    $("#hack-back-side").slideUp(300);
+    $("#make").hide(300);
+    $("#hack").show(300);
+    $("#make-back-side").slideDown(300);
+  });
+});
+
+/* document.getElementById('hack-side').addEventListener("click", function() {
   console.log("hi");
-})
+}) */
