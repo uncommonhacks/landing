6e77fa144e18e3b416e45a0488eb1c2c23878914
laudiacay <laudiacay@gmail.com>
2017-10-20 19:50:04 -0500

    ugh

--- js/moving.js
+++ js/moving.js
@@ -1,25 +1,27 @@
-$(document).ready(function(){
-  $("#make-side").hover(
-  function(){
-    $("#make-back-side").slideUp(300);
-    $("#make").show(300);
-  },
-  function(){
-    $("#make").hide(300);
-    $("#make-back-side").slideDown(300);
-  }
-  );
+$(document).ready(function(){    
+  if (screen.width >= 400){
+    $("#make-side").hover(
+      function(){
+        $("#make-back-side").slideUp(300);
+        $("#make").show(300);
+      },
+      function(){
+        $("#make").hide(300);
+        $("#make-back-side").slideDown(300);
+       }
+    );
 
-  $("#hack-side").hover(
-  function(){
-    $("#hack-back-side").slideUp(300);
-    $("#hack").show(300);
-  },
-  function(){
-    $("#hack").hide(300);
-    $("#hack-back-side").slideDown(300);
+    $("#hack-side").hover(
+      function(){
+        $("#hack-back-side").slideUp(300);
+        $("#hack").show(300);
+      },
+      function(){
+        $("#hack").hide(300);
+        $("#hack-back-side").slideDown(300);
+      }
+    );
   }
-  );
 });
 
 /* document.getElementById('hack-side').addEventListener("click", function() {
